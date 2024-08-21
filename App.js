e068794514b0d11d6a3546d0d16303eebb8590a9
Abhishek <bhardwajabhishek140@gmail.com>
2024-08-21 15:45:26 +0530

    h3 added

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ const parent = React.createElement("div", { id: "Parent" }, [
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {},"I am h1 tag" ),
     React.createElement("h2", {},"I am h2 tag" ),
+    React.createElement("h3", {},"I am h3 tag" ),
   ]),
 ]);
 
