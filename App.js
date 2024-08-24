f39db44d13640c1a20bee8f7b7cd534e448e9c96
Abhishek <bhardwajabhishek140@gmail.com>
2024-08-24 18:57:49 +0530

    changes in index.html , app.js, package.json, package lock

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyz: "abc" },
-//   "hello world from react"
-// );
-const parent = React.createElement("div", { id: "Parent" }, [
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "Parents" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {},"I am h1 tag" ),
     React.createElement("h2", {},"I am h2 tag" ),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {},"I am h1 tag" ),
-    React.createElement("h2", {},"I am h2 tag" ),
-    React.createElement("h3", {},"I am h3 tag" ),
-  ]),
+  
 ]);
+const  jsxheading = <h1 id= "Parent">this is jsx</h1>
 
-console.log(parent); // object
+console.log(jsxheading); // object
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxheading);
