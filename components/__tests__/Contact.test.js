93fc4a80ee7ed70100add13989b77aad05450bdd
Abhishek <bhardwajabhishek140@gmail.com>
2024-10-05 18:54:29 +0530

    testing using jest

---
+++ components/__tests__/Contact.test.js
@@ -0,0 +1,9 @@
+import Contact from "../Contact"
+import {render,screen} from "@testing-library/react"
+import "@testing-library/jest-dom"
+test("should load the contact us component",()=> {
+
+    render(<Contact/>);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+});
